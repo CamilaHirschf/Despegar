e3424ad9c8051189111a2c82fddba9dd19f3935b
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-04-12 15:15:53 +0000

    Add files via upload

---
+++ getSecrets.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+access_key_regex = re.compile(r'((?:ASIA|AKIA|AROA|AIDA|ABIA|ACCA|AGPA|AIPA|ANPA|ANVA|APKA|ASCA)([A-Z0-7]{16}))')
+secret_access_key_regex = re.compile(r'[A-Za-z0-9/\+=]{40}')
+gpat_regex = re.compile(r'ghp_[0-9a-zA-Z]{36}')
+
+RED = '\033[31m'
+RESET = '\033[0m'
+GRAY = '\033[90m'
+
+def search_matches(file, regex, type_key):
+    with open(file, 'r', encoding='utf-8', errors='ignore') as f:
+        for num_line, line in enumerate(f, start=1):
+            match = regex.search(line)
+            if match:
+                found_key = match.group()
+                print(f"{type_key} encontrada en: {file}, línea {num_line}: {RED}{found_key}{RESET}\n")
+
+def search_in_repository(path_repository):
+    for root, dirs, files in os.walk(path_repository):
+        for file in files:
+            file_path = os.path.join(root, file)
+            search_matches(file_path, access_key_regex, f"{GRAY}Clave de Acceso AWS{RESET}")
+            search_matches(file_path, secret_access_key_regex, f"{GRAY}Clave Secreta de Acceso AWS{RESET}")
+            search_matches(file_path, gpat_regex, f"{GRAY}Token de Acceso Personal Github{RESET}")
+
+# Cambiar 'ruta/a/tu/proyecto' por la ruta real de tu proyecto local de GitHub Ej: 'C:/Users/User/Documentos/Despegar'
+search_in_directory = 'C:/Users/gimen/OneDrive/Documentos/Despegar/Despegar'
+search_in_repository(search_in_directory)
+
+
+
+
