f71f2836e1f67c394822a575fa0067b8114cbfad
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-04-12 13:13:21 -0300

    Update getSecrets.py

--- getSecrets.py
+++ getSecrets.py
@@ -10,7 +10,8 @@ RESET = '\033[0m'
 GRAY = '\033[90m'
 
 def search_matches(file, regex, type_key):
-    with open(file, 'r', encoding='utf-8', errors='ignore') as f:
+    managed_file = os.path.normpath(file)
+    with open(managed_file, 'r', encoding='utf-8', errors='ignore') as f:
         for num_line, line in enumerate(f, start=1):
             match = regex.search(line)
             if match:
@@ -19,14 +20,18 @@ def search_matches(file, regex, type_key):
 
 def search_in_repository(path_repository):
     for root, dirs, files in os.walk(path_repository):
+        if '.git' in dirs:
+            dirs.remove('.git')
         for file in files:
             file_path = os.path.join(root, file)
+            if '.git' in file_path:
+                continue
             search_matches(file_path, access_key_regex, f"{GRAY}Clave de Acceso AWS{RESET}")
             search_matches(file_path, secret_access_key_regex, f"{GRAY}Clave Secreta de Acceso AWS{RESET}")
-            search_matches(file_path, gpat_regex, f"{GRAY}Token de Acceso Personal Github{RESET}")
+            search_matches(file_path, gpat_regex, f"{GRAY}Token de Acceso Personal GPAT{RESET}")
 
 # Cambiar 'ruta/a/tu/proyecto' por la ruta real de tu proyecto local de GitHub Ej: 'C:/Users/User/Documentos/Despegar'
-search_in_directory = 'ruta/a/tu/proyecto'
+search_in_directory = './'
 search_in_repository(search_in_directory)
 
 
